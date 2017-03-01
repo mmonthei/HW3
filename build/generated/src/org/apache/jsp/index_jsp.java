6b6d834b3f7cb0239ff84c9570291dd0fcea4d82
McKayla Monthei <mckayla-monthei@uiowa.edu>
2017-03-01 09:14:29 -0600

    Added external style sheet and modified the look of salaryCalc

--- build/generated/src/org/apache/jsp/index_jsp.java
+++ build/generated/src/org/apache/jsp/index_jsp.java
@@ -46,7 +46,7 @@ public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
       out.write("<html>\n");
       out.write("    <head>\n");
       out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
-      out.write("        <link rel=\"stylesheet\" href=\"../../style.css\" type=\"text/cess\">\n");
+      out.write("        <link rel=\"stylesheet\" href=\"style.css\" type=\"text/cess\">\n");
       out.write("        <title> Simple Salary Calculator </title>\n");
       out.write("    </head>    \n");
       out.write("    <body>\n");
--- web/index.jsp
+++ web/index.jsp
@@ -3,7 +3,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <link rel="stylesheet" href="../../style.css" type="text/cess">
+        <link rel="stylesheet" type="text/css" href="./style.css">
         <title> Simple Salary Calculator </title>
     </head>    
     <body>
